6e4d2f65aefbefdf47709fe6f367b4f1b65375ac
JefferssonGarzon <jeffersson.garzon@utp.edu.co>
2021-06-15 12:25:57 -0500

    fix deploy H v1.1.1

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('/dist/ParkingLot-Web'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/ParkingLot-Web'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ParkingLot-Web/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ParkingLot-Web/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
